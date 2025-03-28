50b5d90d69b39906d2ad2a10e5c42a2207b12e11
Vlad <rastorguevvlad04@gmail.com>
2025-03-28 23:21:27 +0300

    add src

---
+++ src/calculator.h
@@ -0,0 +1,12 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+class Calculator {
+public:
+    float add(float a, float b);
+    float subtract(float a, float b);
+    float multiply(float a, float b);
+    float divide(float a, float b);
+};
+
+#endif
