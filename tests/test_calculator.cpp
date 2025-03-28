d50234e52b91ecbd484d1fefe2f0554ddb35b554
Vlad <rastorguevvlad04@gmail.com>
2025-03-28 23:28:11 +0300

    added al of tests

--- tests/test_calculator.cpp
+++ tests/test_calculator.cpp
@@ -2,15 +2,16 @@
 #include <catch2/catch.hpp>
 #include "../src/calculator.h"
 
-TEST_CASE("Addition", "[calc]") {
+TEST_CASE("Calculator operations", "[calc]") {
     Calculator calc;
-    REQUIRE(calc.add(2, 3) == 5);
-    REQUIRE(calc.add(-1, 1) == 0);
-    REQUIRE(calc.add(0, 0) == 0);
-}
 
-TEST_CASE("Division", "[calc]") {
-    Calculator calc;
-    REQUIRE(calc.divide(6, 3) == 2.0f);
-    REQUIRE_THROWS_AS(calc.divide(1, 0), const char*);
+    SECTION("Addition") {
+        REQUIRE(calc.add(2, 3) == 5);
+        REQUIRE(calc.add(-1, 1) == 0);
+    }
+
+    SECTION("Division") {
+        REQUIRE(calc.divide(6, 3) == 2.0f);
+        REQUIRE_THROWS_AS(calc.divide(1, 0), const char*);
+    }
 }
