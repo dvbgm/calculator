9c3af4fa99b19b92437d1a5d14c85b80f40221f8
dvbgm <65855393+dvbgm@users.noreply.github.com>
2025-04-03 23:14:03 +0300

    Update tests.cpp

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,15 +1,17 @@
- #define BOOST_TEST_MODULE CalcTests
- #include <boost/test/unit_test.hpp>
- #include "../src/config6.cpp" // Подключаем исходный файл для тестирования
- 
- BOOST_AUTO_TEST_SUITE(BasicOperationsTests)
- 
- BOOST_AUTO_TEST_CASE(Tests) {
-    BOOST_CHECK_EQUAL(add({0.5, 0.5}), 1.0);
-    BOOST_CHECK_EQUAL(subtract({10, 2}), 8);
-    BOOST_CHECK_EQUAL(multiply({2, 12}), 24);
+#define BOOST_TEST_MODULE CalculatorTests
+#include <boost/test/unit_test.hpp>
+#include "../include/operations.h"
+
+BOOST_AUTO_TEST_SUITE(OperationsTests)
+
+BOOST_AUTO_TEST_CASE(TestAdd) {
+    BOOST_CHECK_EQUAL(add({1, 2, 3}), 6);
+    BOOST_CHECK_EQUAL(add({-1, 1}), 0);
+}
+
+BOOST_AUTO_TEST_CASE(TestDivide) {
     BOOST_CHECK_EQUAL(divide({10, 2}), 5);
-    BOOST_CHECK_THROW(divide({10, 0}), runtime_error);
+    BOOST_CHECK_THROW(divide({10, 0}), std::runtime_error);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
