fc06019ea6a2152d2efbb815df6b47c86752937d
dvbgm <65855393+dvbgm@users.noreply.github.com>
2025-04-03 21:21:08 +0300

    Update test_calculator.cpp

--- tests/test_calculator.cpp
+++ tests/test_calculator.cpp
@@ -1,13 +1,31 @@
 #define BOOST_TEST_MODULE CalculatorTests
 #include <boost/test/unit_test.hpp>
-#include "../src/config6.cpp"  // Подключаем тестируемый код
+#include "../src/config6.cpp"
 
-BOOST_AUTO_TEST_CASE(AdditionTest) {
-    std::vector<double> args = {1, 2, 3};
-    BOOST_CHECK_EQUAL(add(args), 6);
+BOOST_AUTO_TEST_SUITE(CalculatorTests)
+
+BOOST_AUTO_TEST_CASE(Addition) {
+    BOOST_CHECK_EQUAL(add({1, 2, 3}), 6);
+    BOOST_CHECK_EQUAL(add({-1, 1}), 0);
 }
 
-BOOST_AUTO_TEST_CASE(DivisionByZeroTest) {
-    std::vector<double> args = {1, 0};
-    BOOST_CHECK_THROW(divide(args), std::runtime_error);
+BOOST_AUTO_TEST_CASE(Subtraction) {
+    BOOST_CHECK_EQUAL(subtract({10, 2, 3}), 5);
 }
+
+BOOST_AUTO_TEST_CASE(Multiplication) {
+    BOOST_CHECK_EQUAL(multiply({2, 3, 4}), 24);
+}
+
+BOOST_AUTO_TEST_CASE(Division) {
+    BOOST_CHECK_CLOSE(divide({10, 2}), 5.0, 0.001);
+    BOOST_CHECK_THROW(divide({1, 0}), runtime_error);
+}
+
+BOOST_AUTO_TEST_CASE(ArgumentParsing) {
+    auto args = parse_args("1.5,2,3.7");
+    BOOST_CHECK_EQUAL(args.size(), 3);
+    BOOST_CHECK_CLOSE(args[0], 1.5, 0.001);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
