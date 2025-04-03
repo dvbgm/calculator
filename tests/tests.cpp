64a95432dacc304f55c7a3648073b5dea5e9b8fc
dvbgm <65855393+dvbgm@users.noreply.github.com>
2025-04-03 22:55:59 +0300

    Update tests.cpp

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,5 +1,5 @@
  #define BOOST_TEST_MODULE CalcTests
- include <boost/test/unit_test.hpp>
+ #include <boost/test/unit_test.hpp>
  #include "../src/config6.cpp" // Подключаем исходный файл для тестирования
  
  BOOST_AUTO_TEST_SUITE(BasicOperationsTests)
