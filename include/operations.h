4657e209623aea7746a0a0a24e4b65dd904be95f
dvbgm <65855393+dvbgm@users.noreply.github.com>
2025-04-03 23:09:33 +0300

    Create operations.h

---
+++ include/operations.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <vector>
+
+// Объявления функций операций
+double add(const std::vector<double>& args);
+double subtract(const std::vector<double>& args);
+double multiply(const std::vector<double>& args);
+double divide(const std::vector<double>& args);
